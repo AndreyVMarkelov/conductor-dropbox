3d2e9d5d260674cf85ff0b8a39aaeafb4df1e2a4
Andrey Markelov <andreymarkelov@dropbox.com>
2026-08-25 09:17:15 -0700

    fix mockito and start message

--- src/main/java/com/dropbox/conductor/error/DropboxErrorMapper.java
+++ src/main/java/com/dropbox/conductor/error/DropboxErrorMapper.java
@@ -5,6 +5,7 @@ import com.dropbox.core.DbxException;
 import com.dropbox.core.RateLimitException;
 import com.dropbox.core.v2.files.CreateFolderErrorException;
 import com.dropbox.core.v2.files.DeleteErrorException;
+import com.dropbox.core.v2.files.RelocationErrorException;
 
 public final class DropboxErrorMapper {
 
@@ -27,6 +28,56 @@ public final class DropboxErrorMapper {
 
         if (exception instanceof RateLimitException) {
             return error(DropboxErrorCode.RATE_LIMITED, "Dropbox rate limit exceeded", true, operation);
+        }
+
+        if (exception instanceof RelocationErrorException relocationException) {
+            var error = relocationException.errorValue;
+            if (error.isFromLookup() && error.getFromLookupValue().isNotFound()) {
+                return error(DropboxErrorCode.PATH_NOT_FOUND, "Dropbox source path does not exist", false, operation);
+            }
+
+            if (error.isTo() && error.getToValue().isConflict()) {
+                return error(
+                        DropboxErrorCode.PATH_CONFLICT, "Dropbox destination path already exists", false, operation);
+            }
+
+            if ((error.isFromWrite() && error.getFromWriteValue().isNoWritePermission())
+                    || (error.isTo() && error.getToValue().isNoWritePermission())) {
+                return error(DropboxErrorCode.PERMISSION_DENIED, "Dropbox path permission denied", false, operation);
+            }
+
+            if (error.isInternalError()) {
+                return error(
+                        DropboxErrorCode.TEMPORARY_UNAVAILABLE,
+                        "Dropbox move failed with a temporary internal error",
+                        true,
+                        operation);
+            }
+
+            if (error.isInsufficientQuota()) {
+                return error(DropboxErrorCode.QUOTA_EXCEEDED, "Insufficient Dropbox storage quota", false, operation);
+            }
+
+            if (error.isTooManyFiles()) {
+                return error(DropboxErrorCode.INVALID_INPUT, "Move contains too many files", false, operation);
+            }
+
+            if (error.isCantMoveFolderIntoItself() || error.isDuplicatedOrNestedPaths()) {
+                return error(DropboxErrorCode.INVALID_INPUT, "Invalid Dropbox move", false, operation);
+            }
+
+            if (error.isCantCopySharedFolder()
+                    || error.isCantNestSharedFolder()
+                    || error.isCantTransferOwnership()
+                    || error.isCantMoveSharedFolder()
+                    || error.isCantMoveIntoVault()
+                    || error.isCantMoveIntoFamily()) {
+                return error(
+                        DropboxErrorCode.OPERATION_NOT_SUPPORTED,
+                        "Dropbox does not allow this move operation",
+                        false,
+                        operation);
+            }
         }
 
         if (exception instanceof DbxApiException apiException) {
